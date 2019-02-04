15074cde7050571fd2f8689bc6d1107ef99806fc
Yousef Kbeili <ykbeili@gmail.com>
2019-02-03 23:00:23 -0600

    Removing omniauth from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,11 @@ class User < ApplicationRecord
    has_secure_password
    validates :first_name, :last_name, presence: true
    
-   VALID_EMAIL_REGEX 
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+   validates :email,
+   presence: true,
+   uniqueness: true,
+   format: VALID_EMAIL_REGEX
 
    def full_name
    "#{first_name} #{last_name}"
